d6129bcbb8227e1dc965b3c88ca937265f50fd80
Ronald19990527 <ronaldhernandezdp@unimagdalena.edu.co>
2024-01-10 21:24:40 -0500

    Bonsai and Pricing Clon

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ let yearlyText = document.querySelector("#yearly-text");
 let theBestPlanForYourBusinessCard = document.querySelector('#the-best-plan-for-your-business-card');
 let theBestPlanForYourBusinessCardTwo = document.querySelector('#the-best-plan-for-your-business-card-two');
 let section2 = document.querySelector("#section-2");
+var iterarAccordionCollapsed;
+var accordionCollapsed = document.getElementsByClassName("accordion-collapsed");
 
     window.addEventListener('scroll', function () {
       let ubicacionSiguiente = window.pageYOffset;
@@ -146,3 +148,192 @@ let section2 = document.querySelector("#section-2");
         theBestPlanForYourBusinessCardTwo.style.opacity = "0";
       }
     })
+
+    function functionCollapsedComparisons() {
+      if (document.getElementById('collapsed-comparisons').style.display === 'none') {
+        document.getElementById('collapsed-comparisons').style.display = 'block';
+
+        document.getElementById('comparisons').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('comparisons').style.paddingLeft = "15px";
+
+        document.getElementById('comparisons').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-comparisons').style.display = 'none';
+
+        document.getElementById('comparisons').style.backgroundColor = "transparent";
+
+        document.getElementById('comparisons').style.paddingLeft = "0px";
+
+        document.getElementById('comparisons').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedProposals() {
+      if (document.getElementById('collapsed-proposals').style.display === 'none') {
+        document.getElementById('collapsed-proposals').style.display = 'block';
+
+        document.getElementById('proposals').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('proposals').style.paddingLeft = "15px";
+
+        document.getElementById('proposals').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-proposals').style.display = 'none';
+
+        document.getElementById('proposals').style.backgroundColor = "transparent";
+
+        document.getElementById('proposals').style.paddingLeft = "0px";
+
+        document.getElementById('proposals').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedContracts() {
+      if (document.getElementById('collapsed-contracts').style.display === 'none') {
+        document.getElementById('collapsed-contracts').style.display = 'block';
+
+        document.getElementById('contracts').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('contracts').style.paddingLeft = "15px";
+
+        document.getElementById('contracts').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-contracts').style.display = 'none';
+
+        document.getElementById('contracts').style.backgroundColor = "transparent";
+
+        document.getElementById('contracts').style.paddingLeft = "0px";
+
+        document.getElementById('contracts').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedInvoices() {
+      if (document.getElementById('collapsed-invoices').style.display === 'none') {
+        document.getElementById('collapsed-invoices').style.display = 'block';
+
+        document.getElementById('invoices').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('invoices').style.paddingLeft = "15px";
+
+        document.getElementById('invoices').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-invoices').style.display = 'none';
+
+        document.getElementById('invoices').style.backgroundColor = "transparent";
+
+        document.getElementById('invoices').style.paddingLeft = "0px";
+
+        document.getElementById('invoices').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedForms() {
+      if (document.getElementById('collapsed-forms').style.display === 'none') {
+        document.getElementById('collapsed-forms').style.display = 'block';
+
+        document.getElementById('forms').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('forms').style.paddingLeft = "15px";
+
+        document.getElementById('forms').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-forms').style.display = 'none';
+
+        document.getElementById('forms').style.backgroundColor = "transparent";
+
+        document.getElementById('forms').style.paddingLeft = "0px";
+
+        document.getElementById('forms').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedQuotes() {
+      if (document.getElementById('collapsed-quotes').style.display === 'none') {
+        document.getElementById('collapsed-quotes').style.display = 'block';
+
+        document.getElementById('quotes').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('quotes').style.paddingLeft = "15px";
+
+        document.getElementById('quotes').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-quotes').style.display = 'none';
+
+        document.getElementById('quotes').style.backgroundColor = "transparent";
+
+        document.getElementById('quotes').style.paddingLeft = "0px";
+
+        document.getElementById('quotes').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedScopesOfWork() {
+      if (document.getElementById('collapsed-scopes-of-work').style.display === 'none') {
+        document.getElementById('collapsed-scopes-of-work').style.display = 'block';
+
+        document.getElementById('scopes-of-work').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('scopes-of-work').style.paddingLeft = "15px";
+
+        document.getElementById('scopes-of-work').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-scopes-of-work').style.display = 'none';
+
+        document.getElementById('scopes-of-work').style.backgroundColor = "transparent";
+
+        document.getElementById('scopes-of-work').style.paddingLeft = "0px";
+
+        document.getElementById('scopes-of-work').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedAgreements() {
+      if (document.getElementById('collapsed-agreements').style.display === 'none') {
+        document.getElementById('collapsed-agreements').style.display = 'block';
+
+        document.getElementById('agreements').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('agreements').style.paddingLeft = "15px";
+
+        document.getElementById('agreements').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-agreements').style.display = 'none';
+
+        document.getElementById('agreements').style.backgroundColor = "transparent";
+
+        document.getElementById('agreements').style.paddingLeft = "0px";
+
+        document.getElementById('agreements').style.paddingTop = "0px";
+      }
+    }
+
+    function functionCollapsedBriefs() {
+      if (document.getElementById('collapsed-briefs').style.display === 'none') {
+        document.getElementById('collapsed-briefs').style.display = 'block';
+
+        document.getElementById('briefs').style.backgroundColor = "#f0f5ef";
+
+        document.getElementById('briefs').style.paddingLeft = "15px";
+
+        document.getElementById('briefs').style.paddingTop = "10px";
+      }
+      else {
+        document.getElementById('collapsed-briefs').style.display = 'none';
+
+        document.getElementById('briefs').style.backgroundColor = "transparent";
+
+        document.getElementById('briefs').style.paddingLeft = "0px";
+
+        document.getElementById('briefs').style.paddingTop = "0px";
+      }
+    }
